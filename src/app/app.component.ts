e8b0efc7e2377dea9f31f5ea8992648ccbf911ff
Kassandra <Fannygalez@outlook.com>
2025-02-03 21:21:48 -0600

    editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'editor';
+  isTextareaEnable: boolean = false;
+
+  blogText: string = "";
+
+  footerDate: number = new Date ().getFullYear();
+
+  //Valores seleccionados
+  selectedFront: string = "Arial";
+  selectedSize: number = 12;
+  selectedColor: string = "black";
+
+  //Habilidad textarea
+  enableTextarea(): void {
+    this.isTextareaEnable = true;
+  }
 }
